952a0e5f1811fe7f55d20f7c53e3f0112200e519
Erick Rosero <erickros45@gmail.com>
2022-07-02 09:51:50 -0500

    Use of decoratetors in functions

---
+++ decorators.py
@@ -0,0 +1,30 @@
+from datetime import date, datetime
+
+def execution_time(func):
+    def wrapper(*args, **kwargs):
+        initial_time = datetime.now()
+        func(*args, **kwargs)
+        final_time = datetime.now()
+        time_enlapsed = final_time - initial_time
+        print("Pasaron: " + str(time_enlapsed.total_seconds()) + " segundos")
+    return wrapper
+
+@execution_time
+def random_func():
+    for _ in range(1, 100000000):
+        pass
+
+@execution_time
+def suma (a: int, b:int):
+    return a + b 
+
+@execution_time
+def saludo(nombre: str):
+    print("Hola " + nombre)
+
+
+random_func()
+suma(5,5)
+saludo("Facundo")
+
+
